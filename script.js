987119a89bdc4bbe3140ebff00637e4063f97a20
KettyPe <86797322+KettyPe@users.noreply.github.com>
2023-08-06 23:20:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,77 @@
-
 const slider = document.querySelector('.slider');
-const before = slider.querySelector('.before');
-const beforeImg = before.querySelector('img');
-const change = slider.querySelector('.change');
+const before = document.querySelector('.before');
+const beforeImage = before.querySelector('img');
+const change = document.querySelector('.change');
 const body = document.body;
 
 let isActive = false;
 
 document.addEventListener('DOMContentLoaded', () => {
-     let width = slider.offsetWidth;
-     beforeImg.style.width = `${width}px`;
+	let width = slider.offsetWidth;
+	beforeImage.style.width = `${width}px`;
 });
 
-const beforeAfterSlider = (x) => {
-     let shift = Math.max(0, Math.min(x, slider.offsetWidth));
-
-     before.style.width = `${shift}px`;
-     change.style.left = `${shift}px`;
-};
-
-const pauseEvent = (e) => {
-     e.stopPropagation();
-     e.preventDefault();
-     return false;
-}
+change.addEventListener('mousedown', () => {
+	isActive = true;
+});
 
 body.addEventListener('mouseup', () => {
-     isActive = false;
-});
-
-body.addEventListener('mousedown', () => {
-     isActive = true;
+	isActive = false;
 });
 
 body.addEventListener('mouseleave', () => {
-     isActive = false;
+	isActive = false;
 });
+
+const beforeAfterSlider = (x) => {
+	let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+	before.style.width = `${shift}px`;
+	change.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	return false;
+};
 
 body.addEventListener('mousemove', (e) => {
-     if(!isActive) {
-          return;
-     }
+	if (!isActive) {
+		return;
+	}
 
-     let x = e.pageX;
-     x -= slider.getBoundingClientRect().left;
-     beforeAfterSlider(x);
-     pauseEvent(e);
+	let x = e.pageX;
+	x -= slider.getBoundingClientRect().left;
+	beforeAfterSlider(x);
+	pauseEvents(e);
 });
 
-
-body.addEventListener('touchstart', () => {
-     isActive = true;
+change.addEventListener('touchstart', () => {
+	isActive = true;
 });
 
 body.addEventListener('touchend', () => {
-     isActive = false;
+	isActive = false;
 });
 
-body.addEventListener('touchcancle', () => {
-     isActive = false;
+body.addEventListener('touchcancel', () => {
+	isActive = false;
 });
 
 body.addEventListener('touchmove', (e) => {
-     if(!isActive) {
-          return;
-     }
+	if (!isActive) {
+		return;
+	}
 
-     let x;
-     let i;
+  let x;
+  
+  let i;
+  for (i = 0; i < e.changedTouches.length; i++) {
+  	x = e.changedTouches[i].pageX; 
+  }
+  
+  x -= slider.getBoundingClientRect().left;
 
-     for(i = 0; i < e.changedTouches; i++) {
-          x = e.changedTouches[i].pageX;
-     }
-
-     x -= slider.getBoundingClientRect().left;
-
-     beforeAfterSlider(x);
-     pauseEvent(e);
+  beforeAfterSlider(x);
+  pauseEvents(e);
 });
